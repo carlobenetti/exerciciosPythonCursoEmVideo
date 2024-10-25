cdc8685f2130983842a5e9a45ba687f4d1e52552
Carlo <carlobenettiwork@gmail.com>
2024-10-25 15:18:37 -0400

    Recomeço mundo 03
    - Erased all World 3 excercises done.
    - Created aulasMundo03 folder

--- Testes.py
+++ Testes.py
@@ -1,5 +1,27 @@
 varComposta = ('Hamburquer', 'Suco', 'Pizza', 'Pudim')
 
-print(varComposta[])
+
+# Utilizar dos valores de um contador como valor de posição dentro da variável composta:
+
+"""
+dcascasca
+"""
+
+for cont in range(0, len(varComposta)):
+    print(f'Vou comer {varComposta[cont]} na posição {cont}')
+
+
+
+
+for pos, comida in enumerate(varComposta):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+
+
+
+
+
+
+
 
 
