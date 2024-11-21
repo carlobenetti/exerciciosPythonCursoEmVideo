f7c5cd42c6578d2d18527003f53980d723b998d6
Carlo <carlobenettiwork@gmail.com>
2024-11-20 21:04:49 -0400

    update

--- Mundo_3/ex_073.py
+++ Mundo_3/ex_073.py
@@ -1,8 +1,29 @@
-# Crie uma tupla com os 20 primeiros colocados da Tabela do Campeonato Brasileiro, na ordem de colocação. Depois mostre:
-# A) Apenas os 5 primeiros colocados.
-# B) Os últimos 4 colocados.
-# C) Uma lista com os times em ordem alfabética.
-# D) Mostre a posição do Grêmio.
+# Crie uma tupla com os 20 primeiros colocados da Tabela do Campeonato Brasileiro,na ordem de colocação. Depois mostre:
+# (X) Apenas os 5 primeiros colocados.
+# () Os últimos 4 colocados.
+# () Uma lista com os times em ordem alfabética.
+# () Mostre a posição do Grêmio.
+
+
+campeoes = ("Flamengo",    "Internacional",    "Atlético-MG",    "São Paulo",    "Fluminense",    "Grêmio",    "Palmeiras",    "Santos",    "Athletico-PR",    "Bragantino",    "Ceará",    "Corinthians",    "Atlético-GO",    "Bahia",    "Sport",    "Fortaleza",    "Vasco",    "Goiás",    "Coritiba",    "Botafogo",)
+
+print(f'///////////// Os 5 primeiros campeões /////////////')
+print('')
+
+for i in range(0, 5):
+    print(f'O {i+1} campeão foi {campeoes[i]}')
+
+print('')
+print('')
+print(f'///////////// Os últimos 4 colocados foram /////////////')
+print('')
+
+
+# Pegando as últimas 5 posições e seus nomes:
+last_five = [(index + 1, name) for index, name in enumerate(campeoes) if index >= len(campeoes) - 5]
+
+print(last_five)
+
 
 
 
