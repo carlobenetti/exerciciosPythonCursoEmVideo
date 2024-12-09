b4e8ab83dde50a715c89107a6d8dd69af4357c20
Carlo <carlobenettiwork@gmail.com>
2024-12-09 18:35:40 -0400

    Update ex_076.py

--- Mundo_3/ex_076.py
+++ Mundo_3/ex_076.py
@@ -5,3 +5,26 @@
 # Arroz................... R$ 4.60
 
 
+lista_mercado = (
+    'Frango', 3.50, 
+    'Arroz', 4.6, 
+    'Café', 16.50, 
+    'Feijão', 7.80, 
+    'Macarrão', 2.70, 
+    'Leite', 4.0, 
+    'Óleo', 5.5, 
+    'Açúcar', 3.2, 
+    'Sal', 1.8, 
+    'Farinha', 3.0, 
+    'Manteiga', 9.0, 
+    'Queijo', 18.5, 
+    'Presunto', 10.0, 
+    'Tomate', 6.3, 
+    'Alface', 2.0
+)
+
+
+even_values = lista_mercado[1::2]
+odd_values = lista_mercado[2::2]
+
+
