c1a252c4a4df7bdbdac9f4e112c1f6ce088757a1
Carlo <carlobenettiwork@gmail.com>
2025-01-28 00:54:08 -0400

    Update ex_075.py

--- Mundo_3/ex_075.py
+++ Mundo_3/ex_075.py
@@ -4,4 +4,51 @@
 # - Quais foram os números pares.
 # Lembre-se, se um valor não estiver presente. Faça aparecer uma mensagem indicando isso.
 
+valores = []
+counter = 0
+countNines = 0
+
+# Adicionando os valores a lista
+while counter < 4:
+    valores.append(int(input("Digite um valor: ")))
+    counter += 1
+    
+# Tranformando a lista em uma tupla
+tupla = tuple(valores)
+
+# Resultados        
+print(tupla)
+
+# Contando os números noves (9)
+countNines = tupla.count(9)
+print(f'O número 9 (nove) apareceu {countNines} vezes.')
+        
+# Encontrando a posição do número 3        
+if 3 in tupla:
+    print(f'O primeiro três está na posição: {tupla.index(3)+1}.')
+else:
+    print(f'3 não está na tupla.')
+
+
+
+# Separando os valores pares
+pares = []
+
+for num in tupla:
+    if num % 2 == 0:
+        pares.append(num)
+print(f'Os valores pares são {num}')
+
+
+
+
+
+
+
+
+
+
+
+
+
 
