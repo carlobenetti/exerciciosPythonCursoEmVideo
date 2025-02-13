d559a1fe1631a10a2799b686e52a2cf981081e38
Carlo <carlobenettiwork@gmail.com>
2025-02-13 13:54:22 -0400

    Update ex_087.py

---
+++ Mundo_3/ex_087.py
@@ -0,0 +1,49 @@
+# Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado.
+# No final mostre a matriz na tela com a formatação correta. 
+
+# Aprimore o desafio anterior. Mostrando:
+# [] A soma de todos os valores pares digitados.
+# [] A soma dos valores da terceira coluna.
+# [] O maior valor da segunda linha.
+
+
+
+matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+
+for linha in range(0, 3):
+    for coluna in range(0, 3):
+        print(f'{matriz[linha][coluna]}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
