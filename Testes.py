572970bd205fe794824f0d9023a5161611fcba5d
Carlo <carlobenettiwork@gmail.com>
2025-02-17 14:12:27 -0400

    up

--- Testes.py
+++ Testes.py
@@ -14,7 +14,24 @@ for pos, linhas in enumerate(range(0, len(boletim))):
     print(f'{pos} {boletim[linhas][0][0]:^8}  {boletim[linhas][1][0]:^4}  {boletim[linhas][2][0]:^4}    {boletim[linhas][1][0] + boletim[linhas][2][0]:^4}  ')
 print()
 
+continuar = 's'
+exibirAluno = 'n'
 
-
-
-
+while continuar != 'n':
+    exibirAluno = str(input('Deseja exibir um aluno? [S/N]: ')).strip().lower()[0]
+    if exibirAluno == 'n':
+        print('Certo...')
+    if exibirAluno == 's':
+        # Perguntando e exibindo o aluno pedido.
+        aluno = int(input('Qual aluno você deseja exibir? '))
+        if aluno > len(boletim):
+            print('Amigo, Não há tantos alunos assim. Tente novamente.')
+        else:
+            for  pos, linhas in enumerate(range(0, len(boletim))):
+                if linhas == aluno:
+                    print(f'{pos} {boletim[linhas][0][0]:^8}  {boletim[linhas][1][0]:^4}  {boletim[linhas][2][0]:^4}    {boletim[linhas][1][0] + boletim[linhas][2][0]:^4}  ')
+            print()
+    if exibirAluno not in 'ns':
+        print('Desculpe, você não digitou sim ou não. Tente de novo.')
+    continuar = str(input('\nDeseja continuar? [S/N]: ')).strip().lower()[0]
+    
