3947240ab09045b9bd08b91997a12400b73d42d1
Carlo <carlobenettiwork@gmail.com>
2025-02-17 18:41:12 -0400

    up

--- Mundo_3/ex_089.py
+++ Mundo_3/ex_089.py
@@ -4,7 +4,11 @@
 
 boletim = []
 continuar = 's'
+counter = 0
 
+
+
+# Cadastrando o boletim.
 while continuar != 'n':
     # Cadastrando os valores e limpando a lista
     aluno = [[], [], []]
@@ -18,25 +22,39 @@ while continuar != 'n':
     continuar = str(input(f'Quer continuar? [S/N]: ')).strip().lower()[0]
 
 
+# print(boletim)
 
 
+print()
+print(f'No NOME    Nota1  Nota2   MÉDIA')
+print('-'*30)
+for pos, linhas in enumerate(range(0, len(boletim))):
+    print(f'{pos} {boletim[linhas][0][0]:^8}  {boletim[linhas][1][0]:^4}  {boletim[linhas][2][0]:^4}    {boletim[linhas][1][0] + boletim[linhas][2][0]:^4}  ')
+print()
 
+continuar = 's'
+exibirAluno = 'n'
 
-
-
-
-
-# MOSTRANDO O BOLETIM
-print('\n    MOSTRANDO O BOLETIM    ')
-for items in range(0, len(boletim)):
-    for subitems in range(0, 3):
-        for nivel1 in range(0, 3):
-            print(f'{boletim[items][subitems][nivel1]}', end='')
-        print()
+while continuar != 'n':
     print()
-
-
-
-print('\n    BOLETIM CRU    ')
-print(boletim)
-
+    exibirAluno = str(input('Deseja exibir um aluno? [S/N]: ')).strip().lower()[0]
+    if exibirAluno == 'n':
+        print('Certo...')
+    if exibirAluno == 's':
+        # Perguntando e exibindo o aluno pedido.
+        print()
+        aluno = int(input('Qual aluno você deseja exibir? '))
+        if aluno > len(boletim):
+            print()
+            print('Amigo, Não há tantos alunos assim. Tente novamente.')
+        else:
+            for  pos, linhas in enumerate(range(0, len(boletim))):
+                if linhas == aluno:
+                    print(f'{pos} {boletim[linhas][0][0]:^8}  {boletim[linhas][1][0]:^4}  {boletim[linhas][2][0]:^4}    {boletim[linhas][1][0] + boletim[linhas][2][0]:^4}  ')
+            print()
+    if exibirAluno not in 'ns':
+        print()
+        print('Desculpe, você não digitou sim ou não. Tente de novo.')
+    print()
+    continuar = str(input('\nDeseja continuar? [S/N]: ')).strip().lower()[0]
+    
--- Testes.py
+++ Testes.py
@@ -2,36 +2,40 @@
 # [] Mostre um boletim contendo a média de cada um.
 # [] Permita que o usuário possa mostrar as notas de cada aluno individualmente.
 
-boletim = [[['Carlo'], [4.5], [3.7]], [['Mario'], [4], [5.9]], [['Mario'], [2.5], [10]]]
+ficha = []
+
+
+while True:
+    nome =  str(input('Nome: '))
+    nota1 = float(input('Nota 1: ')) 
+    nota2 = float(input('Nota 2: ')) 
+    media = (nota1 + nota2)/2
+
+    ficha.append([nome, [nota1, nota2], media])
+
+    resp = str(input('Quer continuar? [S/N]: '))
+    if resp in 'Nn': 
+        break
+
+
+print('-=-' * 30)
+print(f'{'No.':<4}{"Nome":<10}{'Média:>8'}')
+print('-' * 26)
 
 
 
-counter = 0
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:8.1f}')
 
-print(f'No NOME    Nota1  Nota2   MÉDIA')
-print('-'*30)
-for pos, linhas in enumerate(range(0, len(boletim))):
-    print(f'{pos} {boletim[linhas][0][0]:^8}  {boletim[linhas][1][0]:^4}  {boletim[linhas][2][0]:^4}    {boletim[linhas][1][0] + boletim[linhas][2][0]:^4}  ')
-print()
+while True:
+    print('-'*35)
+    opc = int(input('Mostrar notas de qual aluno: '))
+    if opc == 999:
+        break
+    if opc <= len(ficha-1):
+        print(f'Notas de {ficha[opc][0]} são {ficha[opc][1]}')
+print('<<< VOLTE SEMPRE >>>')
 
-continuar = 's'
-exibirAluno = 'n'
 
-while continuar != 'n':
-    exibirAluno = str(input('Deseja exibir um aluno? [S/N]: ')).strip().lower()[0]
-    if exibirAluno == 'n':
-        print('Certo...')
-    if exibirAluno == 's':
-        # Perguntando e exibindo o aluno pedido.
-        aluno = int(input('Qual aluno você deseja exibir? '))
-        if aluno > len(boletim):
-            print('Amigo, Não há tantos alunos assim. Tente novamente.')
-        else:
-            for  pos, linhas in enumerate(range(0, len(boletim))):
-                if linhas == aluno:
-                    print(f'{pos} {boletim[linhas][0][0]:^8}  {boletim[linhas][1][0]:^4}  {boletim[linhas][2][0]:^4}    {boletim[linhas][1][0] + boletim[linhas][2][0]:^4}  ')
-            print()
-    if exibirAluno not in 'ns':
-        print('Desculpe, você não digitou sim ou não. Tente de novo.')
-    continuar = str(input('\nDeseja continuar? [S/N]: ')).strip().lower()[0]
-    
+
+
