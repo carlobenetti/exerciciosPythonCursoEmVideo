2a14d73f88b6506ccad43996aa600926f7bb3995
Carlo <carlobenettiwork@gmail.com>
2025-04-02 12:31:54 -0400

    Update ex_091.py

--- Mundo_3/ex_091.py
+++ Mundo_3/ex_091.py
@@ -4,13 +4,32 @@
 
 
 from random import randint
+from time import sleep
 
+################## Tabela Cores ####################
+cor_fim = '\033[m'
+vermelho = '\033[1;31m'
+amarelo = '\033[1;33m'
+verde = '\033[1;32m'
+
+preto = '\033[1;30m'
+azul = '\033[1;34m'
+magenta = '\033[1;35m'
+ciano = '\033[1;36m'
+cinza = '\033[1;37m'
+branco = '\033[1;97m'
+
+
+################## Começo do cógido ####################
 tabela = []
+
+print(f'{amarelo}Valores Sorteados:{cor_fim} ')
 
 # Inserindo os valores
 jogadores = {
     'jogador1': randint(0, 4)
 }
+
 
 jogadores['jogador2'] = randint(0, 4)
 while jogadores['jogador2'] == jogadores['jogador1']:
@@ -28,11 +47,43 @@ while jogadores['jogador4'] == jogadores['jogador1'] or jogadores['jogador4'] ==
 tabela.append(jogadores.copy())
 
 
+# Imprimindo o resultado do sorteio de cada jogador.
+for l in tabela:
+    for k, v in jogadores.items():
+        print(f'O jogador {k} tirou {v}')
+        sleep(0.5)
+
+
+# Colocar os números em ordem alfabética
+
+# Posicionando o jogador 1.
+if jogadores['jogador1'] < jogadores['jogador2']:
+    jogadores['jogador1'], jogadores['jogador2'] = jogadores['jogador2'], jogadores['jogador1']
+if jogadores['jogador1'] < jogadores['jogador3']:
+    jogadores['jogador1'], jogadores['jogador3'] = jogadores['jogador3'], jogadores['jogador1']
+if jogadores['jogador1'] < jogadores['jogador4']:
+    jogadores['jogador1'], jogadores['jogador4'] = jogadores['jogador4'], jogadores['jogador1']
+
+# Posicionando o jogador 2.
+if jogadores['jogador2'] < jogadores['jogador3']:
+    jogadores['jogador2'], jogadores['jogador3'] = jogadores['jogador3'], jogadores['jogador2']
+if jogadores['jogador2'] < jogadores['jogador4']:
+    jogadores['jogador2'], jogadores['jogador4'] = jogadores['jogador4'], jogadores['jogador2']
+
+# Posicionando o jogador 2.
+if jogadores['jogador3'] < jogadores['jogador4']:
+    jogadores['jogador3'], jogadores['jogador4'] = jogadores['jogador4'], jogadores['jogador3']
+
+
 
 # Resultado
+print()
+print(f'{verde}Resultado final:{cor_fim} ')
+c = 0
 for val in tabela:
     for k, v in jogadores.items():
-        print(k, v)
+        c += 1
+        print(f'{c}o lugar: {k} com {v} pontos. ')
 
 
 
