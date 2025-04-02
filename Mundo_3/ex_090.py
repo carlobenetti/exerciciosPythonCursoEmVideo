c6a5c01939f89cf19dcd8d72c8017d7ff98f7013
Carlo <carlobenettiwork@gmail.com>
2025-04-02 11:57:36 -0400

    numeros aleatórios OK

--- Mundo_3/ex_090.py
+++ Mundo_3/ex_090.py
@@ -3,6 +3,34 @@
 # No final mostre o conteúdo da estrutura na tela.
 
 
+aluno = {}
+turma = []
+
+
+while True:
+    # Alimentando as variáveis
+    aluno['nome'] = str(input('Digi o nome: '))
+    aluno['media'] = float(input('Digite a média: '))
+    if aluno['media'] >= 7:
+        aluno['situacao'] = 'aprovado'
+    if aluno['media'] < 7:
+        aluno['situacao'] = 'reprovado'
+    turma.append(aluno.copy())
+
+
+    # Fim Loop
+    continuar = str(input('Continuar? [S/N]: ')).strip().lower()[0]
+    if continuar in 'n':
+        break
+
+
+for a in turma:
+    for k, v in a.items():
+        print(f'O {k} é {v}.')
+    print()
+
+
+
 
 
 
--- Mundo_3/ex_091.py
+++ Mundo_3/ex_091.py
@@ -3,5 +3,36 @@
 # No final, coloque esse dicionário em ordem de vitória (sabendo que o vencedor tirou o maior número no dado).
 
 
+from random import randint
+
+tabela = []
+
+# Inserindo os valores
+jogadores = {
+    'jogador1': randint(0, 4)
+}
+
+jogadores['jogador2'] = randint(0, 4)
+while jogadores['jogador2'] == jogadores['jogador1']:
+    jogadores['jogador2'] = randint(0, 4)
+
+jogadores['jogador3'] = randint(0, 4)
+while jogadores['jogador3'] == jogadores['jogador1'] or jogadores['jogador3'] == jogadores['jogador2']:
+    jogadores['jogador3'] = randint(0, 4)
+
+jogadores['jogador4'] = randint(0, 4)
+while jogadores['jogador4'] == jogadores['jogador1'] or jogadores['jogador4'] == jogadores['jogador2'] or jogadores['jogador4'] == jogadores['jogador3']:
+    jogadores['jogador4'] = randint(0, 4)
+
+
+tabela.append(jogadores.copy())
+
+
+
+# Resultado
+for val in tabela:
+    for k, v in jogadores.items():
+        print(k, v)
+
 
 
