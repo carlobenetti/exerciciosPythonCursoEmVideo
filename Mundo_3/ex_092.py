fd484579d92e59c4547dfc6f5b3a13abc56cf86a
Carlo <carlobenettiwork@gmail.com>
2025-04-03 09:22:38 -0400

    up

--- Mundo_3/ex_092.py
+++ Mundo_3/ex_092.py
@@ -1,5 +1,35 @@
-# Um programa que leia nome, ano de nascimento, e carteira de trabalho.
-# Cadastre-os (com idade) em um dicionário 
+# Um programa que 
+# [X] leia nome, ano de nascimento, e carteira de trabalho.
+# [ ] Cadastre-os (com idade) em um dicionário 
 # Se por acaso a CTPS for diferente de zero 
 #   -> O dicionário receberá tambem o ano de contratação e o salário.
-# Calcule e acresente com a idad, com quantos anos a pessoa vai se aposentar.
+# Calcule e acresente com a idade, com quantos anos a pessoa vai se aposentar.
+# OBS: Aposentadoria apenas após 35 anos de contribuição.
+
+from datetime import date
+usuario = {} # Dados da pessoa
+
+
+
+
+
+# usuario['nome'] = str(input('Digite o nome: '))
+usuario['ano_nascimento'] = int(input('O ano de nascimento: '))
+usuario['idade'] = date.today().year - usuario['ano_nascimento']
+# usuario['carteira_trabalho'] = int(input('O número da carteira de trabalho (0 não tem): '))
+# usuario['ano_contratação'] = int(input('O ano de contratação: '))
+# usuario['salario'] = float(input('Digite o salário: '))
+
+
+print(usuario['idade'])
+
+
+
+
+
+
+
+
+
+
+
--- Testes.py
+++ Testes.py
@@ -1,3 +1,10 @@
-for c in range(0, 6):
-    print(f'ex_0{89 + c + 1}.py', end=' ')
+from datetime import date
+usuario = {} # Dados da pessoa
 
+# Pegando o ano atual
+current_date = date.today()
+current_year = current_date.year
+
+
+print(current_year)
+print(current_year - 1999)
