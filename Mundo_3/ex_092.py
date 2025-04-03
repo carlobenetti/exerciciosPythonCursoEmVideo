b2cef88b9ab231ebb7053463f7b4f08863f57183
Carlo <carlobenettiwork@gmail.com>
2025-04-03 12:35:25 -0400

    93 ok

--- Mundo_3/ex_092.py
+++ Mundo_3/ex_092.py
@@ -1,31 +1,45 @@
 # Um programa que 
 # [X] leia nome, ano de nascimento, e carteira de trabalho.
-# [ ] Cadastre-os (com idade) em um dicionário 
-# Se por acaso a CTPS for diferente de zero 
-#   -> O dicionário receberá tambem o ano de contratação e o salário.
-# Calcule e acresente com a idade, com quantos anos a pessoa vai se aposentar.
+# [X] Cadastre-os (com idade) em um dicionário 
+# [X] Se por acaso a CTPS for diferente de zero 
+# [X] -> O dicionário receberá tambem o ano de contratação e o salário.
+# [X] Calcule e acresente com a idade, com quantos anos a pessoa vai se aposentar.
 # OBS: Aposentadoria apenas após 35 anos de contribuição.
 
 from datetime import date
 usuario = {} # Dados da pessoa
+tabela_usuarios = []
+
+while True:
+    # Dados normais
+    usuario['nome'] = str(input('Digite o nome: '))
+    usuario['ano_nascimento'] = int(input('O ano de nascimento: '))
+    usuario['idade'] = date.today().year - usuario['ano_nascimento']
+    usuario['carteira_trabalho'] = int(input('O número da carteira de trabalho (0 não tem): '))
+
+    # Dados de trabalho
+    if usuario['carteira_trabalho'] != 0:
+        usuario['ano_contratação'] = int(input('Digite o ano de contratção: '))
+        usuario['ano_aposentadoria'] = (usuario['ano_contratação'] - usuario['ano_nascimento']) + 35
+        usuario['salario'] = float(input('O salário atual: '))
+    else:
+        usuario['ano_contratação'] = 'nenhum'
+        usuario['salario'] = 0
+        usuario['ano_aposentadoria'] = 'nenhum'
+        print('Usuário sem carteira ativa.')
+
+    tabela_usuarios.append(usuario.copy())    
+    
+    continuar = str(input('Quer continuar? [s/n]: ')).strip().lower()[0]
+    if continuar in 'n':
+        break
 
 
 
-
-
-# usuario['nome'] = str(input('Digite o nome: '))
-usuario['ano_nascimento'] = int(input('O ano de nascimento: '))
-usuario['idade'] = date.today().year - usuario['ano_nascimento']
-# usuario['carteira_trabalho'] = int(input('O número da carteira de trabalho (0 não tem): '))
-# usuario['ano_contratação'] = int(input('O ano de contratação: '))
-# usuario['salario'] = float(input('Digite o salário: '))
-
-
-print(usuario['idade'])
-
-
-
-
+for l in tabela_usuarios:
+    for k, v in l.items():
+        print(f'{k}.......{v}')
+    print()
 
 
 
--- Mundo_3/ex_093.py
+++ Mundo_3/ex_093.py
@@ -2,3 +2,45 @@
 # O programa vai ler o nome do jogador e quantas partidas ele jogou.
 # Depois, ler a quantidade de gols feitos em cada partida.
 # Salvar tudo isso em um dicionário, junto ao total de gols feitos no campeonato inteiro.
+
+
+
+
+jogador = {}
+
+jogador['nome'] = str(input('Digite o nome do jogador: '))
+
+partidas_jogadas = int(input('Quantas partidas ele jogou? '))
+
+jogador['gols'] = []
+
+for i in range(0, partidas_jogadas):
+    jogador['gols'].append(int(input(f'Quantos gols na {i+1} partida? ')))
+
+jogador['total'] = sum(jogador['gols'])
+
+# Detalhe dos dados obtidos
+print()
+for k, v in jogador.items():
+    print(f'O campo {k} tem valor {v}.')    
+
+
+
+# Gol em cada partida
+print()
+for p, v in enumerate(jogador['gols']):
+    print(f'Na partida {p}, fez {v} gols.')
+
+print()
+print(f'Foram {jogador['total']} gols no total.')
+
+
+
+
+
+
+
+
+
+
+
