3a0443de024ec45993398cf73a38cc59846c4652
Carlo <carlobenettiwork@gmail.com>
2025-04-20 11:49:23 -0400

    up

--- Mundo_3/ex_081.py
+++ Mundo_3/ex_081.py
@@ -1,36 +1,16 @@
-# Crie um programa que vai ler vários números e colocar em uma lista:
+# Crie um programa que vai ler vários números e colocar em uma lista. Mostre: 
 # [] Quantos números foram digitados.
 # [] A lista de valores ordenada de forma decrescente.
-# [] Se o valor 5 foi digitado e está na lista ou não.
+# [] Se o valor 5 foi digitado e está ou não na lista
+
+from random import randint
 
 lista = []
-cont = 'S'
-CincoPresente = []
-posCinco = []
 
-
-while cont not in 'Nn':
-    lista.append(int(input('Digite um valor: ')))
-    cont = str(input('Quer continuar? [S/N]: '))
-
-
-for pos, values in enumerate(lista):
-    if values == 5:
-        CincoPresente.append(values)
-        posCinco.append(pos)
+for c in range(0, 5):
+    lista.append(randint(0, 30))
 
 
 
-
-print(f'Os valores digitados foram: {lista}.')
-
-print(f'\nForam digitados {len(lista)} números.'
-f'\nValores em ordem crescente: {sorted(lista)}')
-
-if 5 in lista:
-    print(f'O valor 5 está presente.\nEle apareceu {len(CincoPresente)} vezes. Nas posições: ',end='')
-    for cincos in posCinco:
-        print(f'{cincos}...', end=' ')
-else:
-    print('O valor 5 não estava presente na lista.')
+print(lista)
 
---
+++ Mundo_3/ex_100.py
@@ -0,0 +1,9 @@
+# Um programa que tenha uma lista chamada números 
+# e duas funções chamadas sorteia().
+# A primeira função va isortear 5 números e vai colocalos dentro da lista 
+# e a segunda função vai mostrar a soma entre todos os valoes PARES
+# Sorteados pela funcão anterior.
+
+
+
+ 
