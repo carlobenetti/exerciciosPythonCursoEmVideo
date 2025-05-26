a11f84e1d0ea08832ce347c8cceeadf5fa507018
Carlo <carlobenettiwork@gmail.com>
2025-05-26 14:20:25 -0400

    Update ex_100.py

--- Mundo_3/ex_100.py
+++ Mundo_3/ex_100.py
@@ -8,33 +8,27 @@ from random import randint
 from time import sleep
 
 
-# A primeira função ira sortear 5 números e ira colocalos dentro da lista 
-def sorteando():
-    for items in range(0, 5):
-        novoItem = randint(0, 100)
-        lista.append(novoItem)
-        sleep(0.3)
-        print(f'{novoItem}', end=' ', flush=True)
-    print()
-    print(f'Lista completa: {lista}')
+def sorteia(lista):
+    for cont in range(0, 5):
+        num = randint(0, 10)
+        lista.append(num)
+        sleep(0.25)
+        print(f'{num}', end=' ', flush=True)
 
+def somaPar(lista):
+    soma = 0
+    for valor in lista:
+        if valor % 2 == 0:
+            print(f'{valor}')
+        elif valor % 2 == 1:
+            print(f'{valor} É impar')
+    # print(f'Somando os valores pares de {lista}, temos {soma}')
 
-
-# A segunda função ira mostrar a soma entre todos os valoes PARES
-def somaPar(* num):
-    print(f'Os valores pares são:')
-    for i in num: 
-        sleep(0.3) 
-        print(f'{i}', end=' ', flush=True)
-
-
-
-########## CODIGO REAL ############
-lista = []
-print(f'Sorteando')
-sorteando()
-somaPar(lista)
-
-
-
-        
+    
+# Código principal
+numeros = []
+sorteia(numeros)
+print()
+print(numeros)
+somaPar(numeros)
+    
