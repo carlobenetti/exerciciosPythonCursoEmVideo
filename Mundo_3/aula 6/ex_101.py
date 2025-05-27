c4774919c1145e0c4870717bc7f2b9c51ca372ce
Carlo <carlobenettiwork@gmail.com>
2025-05-27 18:38:39 -0400

    up

--- Mundo_3/aula 6/ex_101.py
+++ Mundo_3/aula 6/ex_101.py
@@ -2,15 +2,20 @@
 # que vai receber como parâmetro o ano de nascimento de uma pessoa 
 # retornando um valor literal indicando se uma pessoa: 
 #   - tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
-import time
-
-ano_candidato = int(input('Em qual ano você nasceu? Ex: 1993 \n->  '))
-ano_atual = time.localtime().tm_year
-
-print(ano_candidato)
 
 
+def voto(ano): 
+    from datetime import date
+    atual = date.today().year
+    idade = atual-ano
+    if idade < 16:
+        return f'Com {idade} anos: Não vota!'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO'
 
+print(voto(2000))
 
 
 
