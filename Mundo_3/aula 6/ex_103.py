30aa3588ffeca8f05a3a76425b35f4238c8b3b8d
Carlo <carlobenettiwork@gmail.com>
2025-12-17 11:23:19 -0400

    Update ex_103.py

--- Mundo_3/aula 6/ex_103.py
+++ Mundo_3/aula 6/ex_103.py
@@ -4,19 +4,18 @@
 # O programa deverá ser capaz de mostrar a ficha do jogador,
 # mesmo que algum dado não tenha sido informado corretamente.
 
-def ficha(jog='<desconhecido>', gols=0):
+def ficha(jog='<desconhecido>', gols='<desconhecido>'):
+    # Pegando as informações do jogador
+    nome = str(input('Nome do jogador: '))
+    gols = str(input('Número de gols: '))
+    if nome.strip() == '':
+        ficha(gols=gols)
+    else:
+        ficha(nome, gols)
+    
+    
     print(f'O jogador {jog} fez {gols} gols(s) no campeonato.')
 
 
-nome = str(input('Nome do jogador: '))
-gols = str(input('Número de gols: '))
-if gols.isnumeric():
-    gols = int(gols)
-else:
-    gols = 0
-if nome.strip() == '':
-    ficha(gols=gols)
-else:
-    ficha(nome, gols)
-    
+ficha()
     
